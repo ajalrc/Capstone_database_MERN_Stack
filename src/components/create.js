9824956b77715dff1a3f3835beb89dabaa2b2a47
ajalrc <ajalrc456@gmail.com>
2022-04-24 12:32:45 -0500

    first commit

---
+++ src/components/create.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+  const [form, setForm] = useState({
+    capstone: "",
+    group_names: "",
+    description: "",
+    presentation_link: "",
+  });
+  const navigate = useNavigate();
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newPerson = { ...form };
+
+    await fetch("http://localhost:5000/record/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPerson),
+    }).catch((error) => {
+      window.alert(error);
+      return;
+    });
+
+    setForm({ capstone: "", group_names: "", description: "" });
+    navigate("/");
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div>
+      <h3>Create Capstone Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="capstone">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="capstone"
+            value={form.capstone}
+            placeholder="Go bulldogs"
+            required
+            onChange={(e) => updateForm({ capstone: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="group_names">Group Names: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="group_names"
+            placeholder="Iron Man, Captain America, Wolverine"
+            value={form.group_names}
+            required
+            onChange={(e) => updateForm({ group_names: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="desription">Description(limit: 200): </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            value={form.description}
+            placeholder="There is where you decribe about your project."
+            maxLength="200"
+            required
+            onChange={(e) => updateForm({ description: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="presentation_link">Presentation Link: </label>
+          <input
+            type="url"
+            className="form-control"
+            id="presentation_link"
+            value={form.presentation_link}
+            pattern="http(s?)(:\/\/) +(drakeedu-my.sharepoint)+(.com)+*((:\/\/) | [a-zA-z0-9\-_]))"
+            placeholder="https://google.com"
+            required
+            onChange={(e) => updateForm({ presentation_link: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Capstone Info"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+
+// Here, we display our Navbar
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          <img
+            //style={{ width: 25 + "%" }}
+            src="https://images.squarespace-cdn.com/content/v1/57961f422994cace5cc0e6a2/1599877840153-OSQYFX9GFVU4BI1IVQP0/Drake+University+Logo+copy.png"
+          ></img>
+        </NavLink>
+
+        <a
+          className="navbar-brand"
+          onClick={() => window.open("https://www.drake.edu/cs/")}
+        >
+          <div className="header">
+            Department of Computer Science and Mathematics
+          </div>
+        </a>
+        <button
+          className="navbar-toggler "
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ml-right">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/create">
+                Capstone Record
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
